06f4795566572eada6f44ffb32388b77144925f7
SongYantao <songyantao@phoenixcontact.com.cn>
2021-05-15 03:09:37 +0800

    ddd

--- AutoAdder/AutoAdder-rt/src/com/pxccn/AutoAdder/BXML_Adder.java
+++ AutoAdder/AutoAdder-rt/src/com/pxccn/AutoAdder/BXML_Adder.java
@@ -13,9 +13,9 @@ import com.tridium.nre.util.IElement;
 import javax.baja.nre.annotations.Facet;
 import javax.baja.nre.annotations.NiagaraAction;
 import javax.baja.nre.annotations.NiagaraType;
+import javax.baja.status.BStatus;
 import javax.baja.sys.*;
 import javax.baja.xml.XElem;
-import javax.baja.xml.XException;
 import javax.baja.xml.XParser;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,50 +30,44 @@ import java.util.logging.Logger;
  */
 
 @NiagaraType
-@NiagaraAction(name = "test", defaultValue = "BString.DEFAULT", parameterType = "BString", facets = {@Facet(name = "BFacets.MULTI_LINE", value = "BBoolean.TRUE")})
+@NiagaraAction(name = "submit", defaultValue = "BString.DEFAULT", parameterType = "BString", facets = {@Facet(name = "BFacets.MULTI_LINE", value = "BBoolean.TRUE")})
 public class BXML_Adder extends BComponent {
 
 
-    /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
-    /*@ $com.pxccn.AutoAdder.BXML_Adder(1713428973)1.0$ @*/
-    /* Generated Fri May 14 14:17:02 CST 2021 by Slot-o-Matic (c) Tridium, Inc. 2012 */
+    
+/*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
+/*@ $com.pxccn.AutoAdder.BXML_Adder(1597672472)1.0$ @*/
+/* Generated Sat May 15 03:07:26 CST 2021 by Slot-o-Matic (c) Tridium, Inc. 2012 */
 
 ////////////////////////////////////////////////////////////////
-// Action "test"
+// Action "submit"
 ////////////////////////////////////////////////////////////////
-
-    /**
-     * Slot for the {@code test} action.
-     *
-     * @see #test(BString parameter)
-     */
-    public static final Action test = newAction(0, BString.DEFAULT, BFacets.make(BFacets.MULTI_LINE, BBoolean.TRUE));
-
-    /**
-     * Invoke the {@code test} action.
-     *
-     * @see #test
-     */
-    public void test(BString parameter) {
-        invoke(test, parameter, null);
-    }
+  
+  /**
+   * Slot for the {@code submit} action.
+   * @see #submit(BString parameter)
+   */
+  public static final Action submit = newAction(0, BString.DEFAULT, BFacets.make(BFacets.MULTI_LINE, BBoolean.TRUE));
+  
+  /**
+   * Invoke the {@code submit} action.
+   * @see #submit
+   */
+  public void submit(BString parameter) { invoke(submit, parameter, null); }
 
 ////////////////////////////////////////////////////////////////
 // Type
 ////////////////////////////////////////////////////////////////
+  
+  @Override
+  public Type getType() { return TYPE; }
+  public static final Type TYPE = Sys.loadType(BXML_Adder.class);
 
-    @Override
-    public Type getType() {
-        return TYPE;
-    }
-
-    public static final Type TYPE = Sys.loadType(BXML_Adder.class);
-
-    /*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
+/*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
 
 
-    public void doTest(BString v) throws Exception {
-        OperateDecoder decoder =new OperateDecoder(v.toString());
+    public void doSubmit(BString v) throws Exception {
+        OperateDecoder decoder = new OperateDecoder(v.toString());
         decoder.readHeader();
         decoder.doIt(getRootComponent());
     }
@@ -90,7 +84,7 @@ public class BXML_Adder extends BComponent {
     public static final String VERSION = "0.01";
 
     public static class OperateDecoder {
-        private OperateElement root;
+        private final OperateElement root;
 
         public OperateDecoder(String xml) throws Exception {
             this.root = OperateElement.make(XParser.make(xml).parse());
@@ -112,8 +106,8 @@ public class BXML_Adder extends BComponent {
             }
         }
 
-        static void doProcess(BValue currentCursor, OperateElement element) {
-            Op.valueOf(element.getOpCode()).doIt.Process(currentCursor, element);
+        static BValue doProcess(BValue currentCursor, OperateElement element) {
+            return Op.valueOf(element.getOpCode()).doIt.Process(currentCursor, element);
         }
 
         static void goNext(BValue currentCursor, OperateElement element) {
@@ -123,33 +117,233 @@ public class BXML_Adder extends BComponent {
         }
 
         enum Op {
+            //创建Component并添加，等同于 addSlot + newComponent
             createComponent((currentCursor, element) -> {
                 CreateComponentElement e = CreateComponentElement.make(element);
-                BValue instance = Util.getNewBValueInstance(e.moduleName, e.typeName);
-                goNext(instance, element);
-                ((BComponent) currentCursor).add(e.slotName, instance, e.flag);
-            });
+                goNext(e.instance, e);
+                ((BComponent) currentCursor).add(e.slotName, e.instance, e.flag);
+                return null;
+            }),
+            //获取指定的Slot
+            getSlot((currentCursor, element) -> {
+                GetSlotElement e = GetSlotElement.make(element);
+                BValue value = ((BComplex) currentCursor).get(e.slotName);
+                if(value == null){
+                    throw new IllegalArgumentException("can not get slot :"+e.slotName);
+                }
+                goNext(value, e);
+                return null;
+            }),
+            //添加Slot,下一层级跟上实例
+            addSlot((currentCursor, element) -> {
+                AddSlotElement e = AddSlotElement.make(element);
+                BValue value = doProcess(currentCursor, e.nextDeepOperateElements().get(0));
+                ((BComponent) currentCursor).add(e.slotName, value, e.flag);
+                return null;
+            }),
+            //对指定的Slot赋值,下一层级跟上实例
+            setSlot((currentCursor, element) -> {
+                SetSlotElement e = SetSlotElement.make(element);
+                BValue value;
+                value = doProcess(currentCursor, e.nextDeepOperateElements().get(0));
+                ((BComplex) currentCursor).set(e.slotName, value);
+                if(e.flag!=null){
+                    ((BComplex) currentCursor).setFlags(((BComplex) currentCursor).getSlot(e.slotName), e.flag);
+                }
+                return null;
+            }),
+            //创建Component，用于配合setValue或addSlot
+            newComponent((currentCursor, element) -> {
+                NewComponentElement e = NewComponentElement.make(element);
+                goNext(e.instance, e);
+                return e.instance;
+            }),
+            //创建已知的Simple,用于配合setValue或addSlot
+            newSimple((currentCursor, element) -> {
+                NewSimpleElement e = NewSimpleElement.make(element);
+                return e.instance;
+            }),
+            setFlag((currentCursor, element) -> {
+                SetFlagElement e = SetFlagElement.make(element);
+                ((BComplex) currentCursor).setFlags(((BComplex) currentCursor).getSlot(e.slotName), e.flag);
+                return null;
+            }),
+
+            ;
 
             final Handler doIt;
+
             Op(Handler doIt) {
                 this.doIt = doIt;
             }
         }
 
         interface Handler {
-            void Process(BValue currentCursor, OperateElement element);
+            BValue Process(BValue currentCursor, OperateElement element);
         }
 
+    }
+
+
+    public static class NewComponentElement extends OperateElement {
+
+        private NewComponentElement(XElem elem) {
+            super(elem);
+            String moduleName = this.get("m", null);
+            String typeName;
+            if (moduleName == null) {
+                String tStr = this.get("t");
+                int pos = tStr.indexOf(":");
+                if (pos < 0) {
+                    throw new RuntimeException("wrong type define !");
+                }
+                moduleName = tStr.substring(0, pos);
+                typeName = tStr.substring(pos + 1);
+            } else {
+                typeName = this.get("t");
+            }
+            this.moduleName = moduleName;
+            this.typeName = typeName;
+            this.instance = Util.getNewBValueInstance(this.moduleName, this.typeName);
+        }
+
+        public static NewComponentElement make(OperateElement elem) {
+            return elem == null ? null : new NewComponentElement(elem.getXmlElement());
+        }
+
+        public final String moduleName;
+        public final String typeName;
+        public final BValue instance;
+    }
+
+    public static class NewSimpleElement extends OperateElement {
+
+        private NewSimpleElement(XElem elem) {
+            super(elem);
+            this.typeName = this.get("t");
+            switch (this.typeName) {
+                case "boolean":
+                case "bool":
+                case "BBoolean":
+                    this.instance = BBoolean.make(this.getb("v"));
+                    break;
+                case "Integer":
+                case "int":
+                case "BInteger":
+                    this.instance = BInteger.make(this.geti("v"));
+                    break;
+                case "String":
+                case "string":
+                case "BString":
+                    this.instance = BString.make(this.get("v"));
+                    break;
+                case "BStatus":
+                    this.instance = BStatus.make(this.geti("v"));
+                    break;
+                default:
+                    this.instance = null;
+            }
+        }
+
+        public static NewSimpleElement make(OperateElement elem) {
+            return elem == null ? null : new NewSimpleElement(elem.getXmlElement());
+        }
+
+        public final String typeName;
+        public final BValue instance;
+    }
+
+
+    public static class SetSlotElement extends OperateElement {
+
+        private SetSlotElement(XElem elem) {
+            super(elem);
+            this.slotName = this.get("n");
+            if (this.geti("f", -1) != -1) {
+                this.flag = this.geti("f");
+            } else {
+                this.flag = null;
+            }
+        }
+
+        public static SetSlotElement make(OperateElement elem) {
+            return elem == null ? null : new SetSlotElement(elem.getXmlElement());
+        }
+
+        public final String slotName;
+        public final Integer flag;
+    }
+
+    public static class SetFlagElement extends OperateElement {
+
+        private SetFlagElement(XElem elem) {
+            super(elem);
+            this.flag = elem.geti("f");
+            this.slotName = elem.get("n");
+        }
+
+        public static SetFlagElement make(OperateElement elem) {
+            return elem == null ? null : new SetFlagElement(elem.getXmlElement());
+        }
+
+        public final int flag;
+        public final String slotName;
+    }
+
+
+    public static class AddSlotElement extends OperateElement {
+
+        private AddSlotElement(XElem elem) {
+            super(elem);
+            this.slotName = this.get("n") + "?";
+            this.flag = this.geti("f", 0);
+
+        }
+
+        public static AddSlotElement make(OperateElement elem) {
+            return elem == null ? null : new AddSlotElement(elem.getXmlElement());
+        }
+
+        public final String slotName;
+        public final int flag;
+    }
+
+    public static class GetSlotElement extends OperateElement {
+
+        private GetSlotElement(XElem elem) {
+            super(elem);
+            this.slotName = this.get("n");
+        }
+
+        public static GetSlotElement make(OperateElement elem) {
+            return elem == null ? null : new GetSlotElement(elem.getXmlElement());
+        }
+
+        public final String slotName;
     }
 
     public static class CreateComponentElement extends OperateElement {
 
         private CreateComponentElement(XElem elem) {
             super(elem);
-            this.moduleName = this.get("m");
-            this.typeName = this.get("t");
-            this.slotName = this.get("n");
-            this.flag = this.geti("f",0);
+            String moduleName = this.get("m", null);
+            String typeName;
+            if (moduleName == null) {
+                String tStr = this.get("t");
+                int pos = tStr.indexOf(":");
+                if (pos < 0) {
+                    throw new RuntimeException("wrong type define !");
+                }
+                moduleName = tStr.substring(0, pos);
+                typeName = tStr.substring(pos + 1);
+            } else {
+                typeName = this.get("t");
+            }
+            this.moduleName = moduleName;
+            this.typeName = typeName;
+            this.slotName = this.get("n") + "?";
+            this.flag = this.geti("f", 0);
+            this.instance = Util.getNewBValueInstance(this.moduleName, this.typeName);
         }
 
         public static CreateComponentElement make(OperateElement elem) {
@@ -160,6 +354,7 @@ public class BXML_Adder extends BComponent {
         public final String typeName;
         public final String slotName;
         public final int flag;
+        public final BValue instance;
     }
 
     public static class OperateElement implements IElement {
@@ -228,6 +423,14 @@ public class BXML_Adder extends BComponent {
 
         public long getl(String attrName, long def) {
             return this.elem.getl(attrName, def);
+        }
+
+        public boolean getb(String attrName, boolean def) {
+            return this.elem.getb(attrName, def);
+        }
+
+        public boolean getb(String attrName) {
+            return this.elem.getb(attrName);
         }
 
         public int attrSize() {
--- AutoAdder/AutoAdder-rt/src/com/pxccn/AutoAdder/testModule/BTest1.java
+++ AutoAdder/AutoAdder-rt/src/com/pxccn/AutoAdder/testModule/BTest1.java
@@ -12,7 +12,9 @@ package com.pxccn.AutoAdder.testModule;
 
 import javax.baja.nre.annotations.NiagaraProperty;
 import javax.baja.nre.annotations.NiagaraType;
+import javax.baja.status.BStatusBoolean;
 import javax.baja.sys.*;
+import javax.baja.util.BFolder;
 import java.util.logging.Logger;
 
 /**
@@ -27,11 +29,13 @@ import java.util.logging.Logger;
 @NiagaraProperty(name = "boolField", type = "boolean", defaultValue = "false")
 @NiagaraProperty(name = "strField", type = "String", defaultValue = "")
 @NiagaraProperty(name = "intField", type = "int", defaultValue = "0")
+@NiagaraProperty(name = "folder", type = "BFolder", defaultValue = "new BFolder()")
+@NiagaraProperty(name = "statusBoolean", type = "BStatusBoolean", defaultValue = "new BStatusBoolean()")
 public class BTest1 extends BComponent {
     
 /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
-/*@ $com.pxccn.AutoAdder.testModule.BTest1(2905170715)1.0$ @*/
-/* Generated Fri May 14 21:45:49 CST 2021 by Slot-o-Matic (c) Tridium, Inc. 2012 */
+/*@ $com.pxccn.AutoAdder.testModule.BTest1(4026516882)1.0$ @*/
+/* Generated Sat May 15 00:16:25 CST 2021 by Slot-o-Matic (c) Tridium, Inc. 2012 */
 
 ////////////////////////////////////////////////////////////////
 // Property "boolField"
@@ -101,6 +105,52 @@ public class BTest1 extends BComponent {
    * @see #intField
    */
   public void setIntField(int v) { setInt(intField, v, null); }
+
+////////////////////////////////////////////////////////////////
+// Property "folder"
+////////////////////////////////////////////////////////////////
+  
+  /**
+   * Slot for the {@code folder} property.
+   * @see #getFolder
+   * @see #setFolder
+   */
+  public static final Property folder = newProperty(0, new BFolder(), null);
+  
+  /**
+   * Get the {@code folder} property.
+   * @see #folder
+   */
+  public BFolder getFolder() { return (BFolder)get(folder); }
+  
+  /**
+   * Set the {@code folder} property.
+   * @see #folder
+   */
+  public void setFolder(BFolder v) { set(folder, v, null); }
+
+////////////////////////////////////////////////////////////////
+// Property "statusBoolean"
+////////////////////////////////////////////////////////////////
+  
+  /**
+   * Slot for the {@code statusBoolean} property.
+   * @see #getStatusBoolean
+   * @see #setStatusBoolean
+   */
+  public static final Property statusBoolean = newProperty(0, new BStatusBoolean(), null);
+  
+  /**
+   * Get the {@code statusBoolean} property.
+   * @see #statusBoolean
+   */
+  public BStatusBoolean getStatusBoolean() { return (BStatusBoolean)get(statusBoolean); }
+  
+  /**
+   * Set the {@code statusBoolean} property.
+   * @see #statusBoolean
+   */
+  public void setStatusBoolean(BStatusBoolean v) { set(statusBoolean, v, null); }
 
 ////////////////////////////////////////////////////////////////
 // Type
